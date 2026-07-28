8d4a347bc6fb7caa6c2a7b74b161d1d569cc23ce
NeXFlowX <nexflowx.tech@gmail.com>
2026-07-28 16:53:38 -0300

    feat(developer): expose merchant API key reveal route

--- src/modules/developer/routes/developer.routes.ts
+++ src/modules/developer/routes/developer.routes.ts
@@ -1,17 +1,55 @@
 import { Router } from 'express';
 
 import * as ctrl from '../controllers/developer.controller';
+import {
+  revealApiKey
+} from '../controllers/api-key-reveal.controller';
 
 const router = Router();
 
-router.get('/api-keys', ctrl.getApiKeys);
-router.post('/api-keys', ctrl.createApiKey);
-router.delete('/api-keys/:id', ctrl.deleteApiKey);
+router.get(
+  '/api-keys',
+  ctrl.getApiKeys
+);
 
-router.get('/webhooks', ctrl.getWebhooks);
-router.post('/webhooks', ctrl.createWebhook);
-router.put('/webhooks/:id', ctrl.updateWebhook);
-router.patch('/webhooks/:id', ctrl.updateWebhook);
-router.delete('/webhooks/:id', ctrl.deleteWebhook);
+router.post(
+  '/api-keys',
+  ctrl.createApiKey
+);
+
+router.post(
+  '/api-keys/:id/reveal',
+  revealApiKey
+);
+
+router.delete(
+  '/api-keys/:id',
+  ctrl.deleteApiKey
+);
+
+router.get(
+  '/webhooks',
+  ctrl.getWebhooks
+);
+
+router.post(
+  '/webhooks',
+  ctrl.createWebhook
+);
+
+router.put(
+  '/webhooks/:id',
+  ctrl.updateWebhook
+);
+
+router.patch(
+  '/webhooks/:id',
+  ctrl.updateWebhook
+);
+
+router.delete(
+  '/webhooks/:id',
+  ctrl.deleteWebhook
+);
 
 export default router;
