1e41120c00814db4593d4fdfbb85f88765de8c97
NeXFlowX <nexflowx.tech@gmail.com>
2026-09-05 17:04:57 -0300

    feat(merchant): expose checkout branding update route

--- src/modules/merchant/routes/merchant.routes.ts
+++ src/modules/merchant/routes/merchant.routes.ts
@@ -4,9 +4,8 @@ import * as merchant from '../controllers/merchant.controller';
 const router = Router();
 
 router.get('/profile', merchant.getProfile);
-
 router.get('/stores', merchant.getStores);
-
 router.get('/stores/:id', merchant.getStore);
+router.put('/stores/:id/checkout-branding', merchant.updateCheckoutBranding);
 
 export default router;
